0865f3c79b5ce2efbb5c7a22589c21d59144f46f
nullifiedcat <nullifiedcat@gmail.com>
2017-08-14 18:49:41 +0300

    fix selection

--- script.js
+++ script.js
@@ -175,58 +175,7 @@ function fetchCGStatus() {
 
 $(() => {
 
-$('select').each(function() {
-	var self = $(this);
-	self.addClass('vgui-select-hidden');
-	self.wrap('<div class="nopad vgui-select-container noselect"></div>');
-	var el = $('<div class="nopad vgui-select"></div>');
-	self.after(el);
-	el.append('<div class="nopad vgui-select-text">Select</div>');
-	var list = $('<ul class="vgui-select-list vgui-outset"></ul>');
-	list.hide();
-	el.on('click', function(event) {
-		event.stopPropagation();
-		$('.vgui-select-active').not(this).each(function() {
-			$(this).removeClass('vgui-select-active');
-			$(this).find('.vgui-select-list').hide();
-		});
-		if (el.hasClass('vgui-select-active')) {
-			el.removeClass('vgui-select-active');
-			list.hide();
-		} else {
-			el.addClass('vgui-select-active');
-			list.show();
-		}
-	});
-	el.append(list);
-	var so = self.find('option')[0];
-	self.find('option').each(function() {
-		if ($(this).attr('selected')) so = $(this);
-		var opt = $('<li />');
-		opt.addClass('vgui-select-option');
-		opt.attr('data-value', $(this).val());
-		opt.text($(this).text());
-		opt.on('click', function() {
-			event.stopPropagation();
-			self.val($(this).attr('data-value'));
-			el.attr('data-value', $(this).text());
-			el.find('.vgui-select-text').text($(this).text());
-			el.removeClass('vgui-select-active');
-			list.hide();
-		});
-		list.append(opt);
-	});
-	if (so) {
-		el.attr('data-value', so.val());
-		el.find('.vgui-select-text').text(so.text());
-	}
-	$(document).on('click', function() {
-		el.removeClass('vgui-select-active');
-		list.hide();
-	});
-});
-
-console.log($("#table-user input"));
+vguiSelect($);
 
 $("#table-user input").on("input", AutoGenerateFields);
 
@@ -304,6 +253,10 @@ $("#cg-login").on("click", function() {
 	var username = $("#cg-acc-login").text();
 	var password = $("#cg-acc-password").text();
 	StartSteam(username, password);
+});
+
+$("#table-user input[type=checkbox]").on('change', function() {
+	$(this).parent().parent().find('[placeholder=RegExp]').prop('disabled', !$(this).prop('checked'))
 });
 
 status = $("#status");
